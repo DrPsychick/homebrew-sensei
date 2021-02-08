d6925cf25ccfa7ed4194b9e71bfc17b632f47a46
DrPsychick <drpsychick@drsick.de>
2021-02-09 00:33:44 +0100

    fix sha256

--- Formula/sensei.rb
+++ Formula/sensei.rb
@@ -2,7 +2,7 @@ class Sensei < Formula
   desc "A small macOS tool to read sensor information"
   homepage "https://github.com/DrPsychick/homebrew-sensei"
   url "https://github.com/DrPsychick/homebrew-sensei/archive/v0.0.2.tar.gz"
-  sha256 "3cc8faa889c948edfdf011be29667b4d0e854d6ba7a91ce260116933732fcf6f"
+  sha256 "0e03f060143baeb321eea0e943708e72ad400bff95b46878fde560a4f1a1c95c"
   license "GPL3"
 
   depends_on "cmake" => :build
