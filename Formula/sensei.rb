a95a0c290eb7ded3a8f20e5b9ab1d7d366e31d67
DrPsychick <drpsychick@drsick.de>
2021-02-09 00:32:11 +0100

    update version

--- Formula/sensei.rb
+++ Formula/sensei.rb
@@ -1,8 +1,8 @@
 class Sensei < Formula
   desc "A small macOS tool to read sensor information"
   homepage "https://github.com/DrPsychick/homebrew-sensei"
-  url "https://github.com/DrPsychick/homebrew-sensei/archive/v0.0.1.tar.gz"
-  sha256 "fbd5fff9a92ef53d78b7af0d237d643c67475c233d791e6f75b1449f56856f0f"
+  url "https://github.com/DrPsychick/homebrew-sensei/archive/v0.0.2.tar.gz"
+  sha256 "3cc8faa889c948edfdf011be29667b4d0e854d6ba7a91ce260116933732fcf6f"
   license "GPL3"
 
   depends_on "cmake" => :build
